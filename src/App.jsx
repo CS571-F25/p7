9b9ab40e8163266af4157c956baa8fb7ca244e50
Jc-42 <jc.julian.carpenter@gmail.com>
2025-11-15 18:57:02 -0600

    Add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { HashRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes, Link } from 'react-router'
 import './App.css'
 import Home from './components/Home'
-import AboutMe from './components/AboutMe'
+import Reservations from './components/Reservations'
+import Restaurants from './components/Restaurants'
 
 function App() {
   return <HashRouter>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/reservations">Reservations</Link>
+      <Link to="/restaurants">Restaurants</Link>
+    </nav>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<AboutMe/>}></Route>
+      <Route path="/reservations" element={<Reservations/>}></Route>
+      <Route path="/restaurants" element={<Restaurants/>}></Route>
     </Routes>
   </HashRouter>
 }
